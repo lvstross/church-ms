16e98b8a6fa17ff18d33def4f3a19577891188cb
Levi Gonzales <lvstross@gmail.com>
2022-08-01 17:52:06 -0600

    fixed mongo connection issue in docker container

--- church-event-service/src/main.ts
+++ church-event-service/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { EventModule } from './event.module';
 
 async function bootstrap() {
+  console.log('Event Service App Init!');
   const app = await NestFactory.create(EventModule);
   await app.listen(3000);
 }
